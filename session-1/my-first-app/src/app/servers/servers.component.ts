59007e1a6f4fbfe4031c49788703482e7d008d66
Rosalito gabriel <rosalitogabriel@solaireresort.com>
2023-03-11 15:59:10 +0800

    Directives and DataBinding

--- session-1/my-first-app/src/app/servers/servers.component.ts
+++ session-1/my-first-app/src/app/servers/servers.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+    allowNewServer: boolean = false;
+    serverCreationStatus: string = 'No server was created!';
+    serverName: string = 'Test Server';
+    serverCreated: boolean = false;
 
+    constructor() {
+        setTimeout(() => { this.allowNewServer = true; }, 2000);
+    }
+
+    ngOnInit() { }
+
+    onCreateServer() {
+        this.serverCreated = true;
+        this.serverCreationStatus = `Server was created! Server Name: ${this.serverName}`;
+    }
+
+    onUpdateServerName(event: Event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
 }
